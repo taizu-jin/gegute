ced050dbcf407513064fe037d4f8a1178b2496a0
taizu-jin <mantas.scc@gmail.com>
2023-06-08 17:22:31 +0300

    feat: set function #[cfg(windows)]

--- src/clock.rs
+++ src/clock.rs
@@ -1,4 +1,5 @@
 use chrono::{DateTime, Local, TimeZone};
+use std::mem::zeroed;
 
 pub struct Clock;
 
@@ -11,7 +12,6 @@ impl Clock {
     pub fn set<Tz: TimeZone>(t: DateTime<Tz>) {
         use libc::{settimeofday, timezone};
         use libc::{suseconds_t, time_t, timeval};
-        use std::mem::zeroed;
 
         let t = t.with_timezone(&Local);
         let mut u: timeval = unsafe { zeroed() };
@@ -22,6 +22,50 @@ impl Clock {
         unsafe {
             let mock_tz: *const timezone = std::ptr::null();
             settimeofday(&u as *const timeval, mock_tz);
+        }
+    }
+
+    #[cfg(windows)]
+    pub fn set<Tz: TimeZone>(t: DateTime<Tz>) {
+        use chrono::{Datelike, Timelike, Weekday};
+        use winapi::shared::minwindef::WORD;
+        use winapi::um::minwinbase::SYSTEMTIME;
+        use winapi::um::sysinfoapi::SetSystemTime;
+
+        let t = t.with_timezone(&Local);
+
+        let mut systime: SYSTEMTIME = unsafe { zeroed() };
+
+        let dow = match t.weekday() {
+            Weekday::Mon => 1,
+            Weekday::Tue => 2,
+            Weekday::Wed => 3,
+            Weekday::Thu => 4,
+            Weekday::Fri => 5,
+            Weekday::Sat => 6,
+            Weekday::Sun => 0,
+        };
+
+        let mut ns = t.nanosecond();
+        let is_leap_second = ns > 1_000_000_000;
+
+        if is_leap_second {
+            ns -= 1_000_000_000;
+        }
+
+        systime.wYear = t.year() as WORD;
+        systime.wMonth = t.month() as WORD;
+        systime.wDayOfWeek = dow as WORD;
+        systime.wDay = t.day() as WORD;
+        systime.wHour = t.hour() as WORD;
+        systime.wMinute = t.minute() as WORD;
+        systime.wSecond = t.second() as WORD;
+        systime.wMilliseconds = (ns / 1_000_000) as WORD;
+
+        let systime_ptr = &systime as *const SYSTEMTIME;
+
+        unsafe {
+            SetSystemTime(systime_ptr);
         }
     }
 }
